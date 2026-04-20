dff885b52cccc9922cd8bbb008bfc22e56767f71
Thakur Shiva Sai <139252198+shivasaithakur@users.noreply.github.com>
2026-04-20 10:23:50 +0530

    Add HTML structure for calculator module documentation

---
+++ assignment_9.1/calculator.py
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Python: module calculator</title>
+</head><body>
+
+<table class="heading">
+<tr class="heading-text decor">
+<td class="title">&nbsp;<br><strong class="title">calculator</strong></td>
+<td class="extra"><a href=".">index</a><br><a href="file:c%3A%5Cusers%5Csrina%5Conedrive%5Cdesktop%5Caiac%5Ccalculator.py">c:\users\srina\onedrive\desktop\aiac\calculator.py</a></td></tr></table>
+    <p><span class="code">#Task&nbsp;3:<br>
+#Create&nbsp;a&nbsp;Python&nbsp;module&nbsp;calculator.py&nbsp;that&nbsp;includes&nbsp;thefollowing&nbsp;functions,&nbsp;each&nbsp;written&nbsp;with&nbsp;appropriate&nbsp;docstrings:&nbsp;o&nbsp;<a href="#-add">add</a>(a,&nbsp;b)&nbsp;–&nbsp;returns&nbsp;the&nbsp;sum&nbsp;of&nbsp;two&nbsp;numberso&nbsp;<a href="#-subtract">subtract</a>(a,&nbsp;b)&nbsp;–&nbsp;returns&nbsp;the&nbsp;difference&nbsp;of&nbsp;two&nbsp;numberso&nbsp;<a href="#-multiply">multiply</a>(a,&nbsp;b)&nbsp;–&nbsp;returns&nbsp;the&nbsp;product&nbsp;of&nbsp;two&nbsp;numberso&nbsp;<a href="#-divide">divide</a>(a,&nbsp;b)&nbsp;–&nbsp;returns&nbsp;the&nbsp;quotient&nbsp;of&nbsp;two&nbsp;numbers.</span></p>
+<p>
+<table class="section">
+<tr class="decor functions-decor heading-text">
+<td class="section-title" colspan=3>&nbsp;<br><strong class="bigsection">Functions</strong></td></tr>
+    
+<tr><td class="decor functions-decor"><span class="code">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span></td><td>&nbsp;</td>
+<td class="singlecolumn"><dl><dt><a name="-add"><strong>add</strong></a>(a, b)</dt><dd><span class="code">Returns&nbsp;the&nbsp;sum&nbsp;of&nbsp;two&nbsp;numbers.<br>
+&nbsp;<br>
+Parameters:<br>
+a&nbsp;(float):&nbsp;The&nbsp;first&nbsp;number.<br>
+b&nbsp;(float):&nbsp;The&nbsp;second&nbsp;number.<br>
+&nbsp;<br>
+Returns:<br>
+float:&nbsp;The&nbsp;sum&nbsp;of&nbsp;a&nbsp;and&nbsp;b.</span></dd></dl>
+ <dl><dt><a name="-divide"><strong>divide</strong></a>(a, b)</dt><dd><span class="code">Returns&nbsp;the&nbsp;quotient&nbsp;of&nbsp;two&nbsp;numbers.<br>
+&nbsp;<br>
+Parameters:<br>
+a&nbsp;(float):&nbsp;The&nbsp;first&nbsp;number.<br>
+b&nbsp;(float):&nbsp;The&nbsp;second&nbsp;number.<br>
+&nbsp;<br>
+Returns:<br>
+float:&nbsp;The&nbsp;quotient&nbsp;of&nbsp;a&nbsp;and&nbsp;b.&nbsp;If&nbsp;b&nbsp;is&nbsp;zero,&nbsp;returns&nbsp;'Error:&nbsp;Division&nbsp;by&nbsp;zero'.</span></dd></dl>
+ <dl><dt><a name="-multiply"><strong>multiply</strong></a>(a, b)</dt><dd><span class="code">Returns&nbsp;the&nbsp;product&nbsp;of&nbsp;two&nbsp;numbers.<br>
+&nbsp;<br>
+Parameters:<br>
+a&nbsp;(float):&nbsp;The&nbsp;first&nbsp;number.<br>
+b&nbsp;(float):&nbsp;The&nbsp;second&nbsp;number.<br>
+&nbsp;<br>
+Returns:<br>
+float:&nbsp;The&nbsp;product&nbsp;of&nbsp;a&nbsp;and&nbsp;b.</span></dd></dl>
+ <dl><dt><a name="-subtract"><strong>subtract</strong></a>(a, b)</dt><dd><span class="code">Returns&nbsp;the&nbsp;difference&nbsp;of&nbsp;two&nbsp;numbers.<br>
+&nbsp;<br>
+Parameters:<br>
+a&nbsp;(float):&nbsp;The&nbsp;first&nbsp;number.<br>
+b&nbsp;(float):&nbsp;The&nbsp;second&nbsp;number.<br>
+&nbsp;<br>
+Returns:<br>
+float:&nbsp;The&nbsp;difference&nbsp;of&nbsp;a&nbsp;and&nbsp;b.</span></dd></dl>
+</td></tr></table>
+</body></html>
